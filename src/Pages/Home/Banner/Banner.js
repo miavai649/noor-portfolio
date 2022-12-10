6f84c024ecb7644fe7e1a5a3de749036ecfc0da3
Mahamudul Haque Noor <mahamudulhaquenoor@gmail.com>
2022-12-10 17:16:14 +0600

    first commit

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import bannerImg from '../../../assets/images/bannerImg.jpg';
+
+const Banner = () => {
+    return (
+        <div className="hero min-h-screen" style={{ backgroundImage: `url(${bannerImg})` }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl text-white font-bold">Welcome to <span className='text-primary'>Fantastic Furniture</span></h1>
+      <p className="mb-5 text-lg text-white">Our Fantastic Furniture website for those people who wants to buy or sell their well home furnishings products and discover the most stylish, contemporary and chic furniture with a low price.</p>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Banner;
---
+++ src/Shared/Navbar/Navbar.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { HiDownload } from "react-icons/hi";
+import { Link } from 'react-router-dom';
+import img from '../../assets/images/noor.png';
+
+const Navbar = () => {
+
+    const menuItem = (
+        <React.Fragment>
+          <li>
+            <Link className='mr-14' to="/">Home</Link>
+          </li>
+          <li>
+            <Link className='mr-14'>My Resume <HiDownload className='text-2xl'></HiDownload></Link>
+          </li>
+          <li>
+            <Link className='mr-14' to="/contact">Contact Me</Link>
+          </li>
+        </React.Fragment>
+      );
+
+    return (
+        <div className="navbar h-20 flex justify-between shadow-xl py-12 text-neutral lg:px-28">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={1}
+            className="menu menu-compact font-semibold dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {menuItem}
+          </ul>
+        </div>
+        <img className='w-14 mr-5 rounded-full' src={img} alt="" />
+        <Link className="normal-case text-2xl font-bold hover:text-primary" to="/">
+          Mahmudul Haque Noor
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal text-lg p-0 font-semibold">{menuItem}</ul>
+      </div>
+    </div>
+    );
+};
+
+export default Navbar;
